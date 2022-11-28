e68031891ca3a153051a0e16dac17c30ca74804a
Rodrigo Lima <limarodrigoo@outlook.com>
2022-11-28 11:21:45 -0300

    Cria testes

--- imovel_app/tests.py
+++ imovel_app/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from imovel_app.models import Imovel
+from rest_framework import status
 
-# Create your tests here.
+
+class ImovelTestCase(APITestCase):
+    def setUp(self):
+        self.list_url = reverse("Imovéis-list")
+        self.imovel1 = Imovel.objects.create(
+            limite_de_hospedes=3, banheiros=1, pet_friendly=False, limpeza=32.2
+        )
+        self.imovel2 = Imovel.objects.create(
+            limite_de_hospedes=6, banheiros=2, pet_friendly=True, limpeza=52.5
+        )
+
+    def test_list_imoveis(self):
+        """Teste para verificar requisiçaõ GET imoveis"""
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(Imovel.objects.count(), 2)
+
+    def test_create_imovel(self):
+        """Test para verificar requisitao POST imoveis"""
+        data = {
+            "limite_de_hospedes": 1,
+            "banheiros": 1,
+            "pet_friendly": False,
+            "limpeza": 30,
+        }
+        response = self.client.post(self.list_url, data=data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
--- reservas_app/tests.py
+++ reservas_app/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from reservas_app.models import Reserva
+from imovel_app.models import Imovel
+from anuncios_app.models import Anuncio
+from rest_framework import status
 
-# Create your tests here.
+
+class ImovelTestCase(APITestCase):
+    def setUp(self):
+        self.list_url = reverse("Reservas-list")
+        self.imovel = Imovel.objects.create(
+            limite_de_hospedes=2,
+            banheiros=1,
+            pet_friendly=False,
+            limpeza=30,
+        )
+        self.anuncio = Anuncio.objects.create(
+            imovel=self.imovel, plataforma="airbnb", taxa_plataforma=59
+        )
+
+    def test_post_reservation(self):
+        wrong_data = {
+            "anuncio_id": 1,
+            "check_in": "2022-11-28",
+            "check_out": "2022-11-21",
+            "preco_total": 199,
+            "comentario": "ok",
+            "qtd_hospedes": 4,
+        }
+        data = {
+            "anuncio_id": 1,
+            "check_in": "2022-11-21",
+            "check_out": "2022-11-28",
+            "preco_total": 199,
+            "comentario": "ok",
+            "qtd_hospedes": 4,
+        }
+        response = self.client.post(self.list_url, data=wrong_data)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        response2 = self.client.post(self.list_url, data=data)
+        print(response2.data)
+        self.assertEquals(response2.status_code, status.HTTP_201_CREATED)
