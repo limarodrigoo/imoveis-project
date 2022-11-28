458fb453fd9786fa399a0bac5c9730f844702172
Rodrigo Lima <limarodrigoo@outlook.com>
2022-11-28 11:20:08 -0300

    Adiciona urls

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from imovel_app.views import ImoveisViewSet
+from anuncios_app.views import AnuncioViewSet
+from reservas_app.views import ReservaViewSet
 
 router = routers.DefaultRouter()
 router.register("imoveis", ImoveisViewSet, basename="Imovéis")
+router.register("anuncios", AnuncioViewSet, basename="Anuncios")
+router.register("reservas", ReservaViewSet, basename="Reservas")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
