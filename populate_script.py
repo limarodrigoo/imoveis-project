c7fb934d4787ef8e2fecb2a251bf1d4b0e0b686c
Rodrigo Lima <limarodrigoo@outlook.com>
2022-11-28 18:53:20 -0300

    popula tabela de reservas

--- populate_script.py
+++ populate_script.py
@@ -6,12 +6,15 @@ django.setup()
 
 from imovel_app.models import Imovel
 from anuncios_app.models import Anuncio
-import uuid
+from reservas_app.models import Reserva
+from faker import Faker
 
 plataformas = ["airbnb", "hoteis.com", "booking.com"]
 
 
 def create_db(qtd_imoveis):
+    Faker.seed(0)
+    fake = Faker('pt-br')
     for _ in range(qtd_imoveis):
         hospedes = random.randint(1, 10)
         banheiros = random.randint(1, 5)
@@ -29,6 +32,23 @@ def create_db(qtd_imoveis):
             taxa = round(random.uniform(50, 300), 2)
             a = Anuncio(imovel=i, plataforma=plataforma, taxa_plataforma=taxa)
             a.save()
+            for _ in range(random.randint(1, 2)):
+                random_month = random.randint(1, 12)
+                random_day = random.randint(1, 21)
+                random_plus_days = random.randint(1, 5)
+                check_in = "2022-%d-%d" % (random_month, random_day)
+                check_out = "2022-%d-%d" % (random_month, random_day + random_plus_days)
+                preco_total = round(random.uniform(120, 999), 2)
+                qtd_hospedes = random.randint(1, hospedes)
+                r = Reserva(
+                    anuncio_id=a,
+                    check_in=check_in,
+                    check_out=check_out,
+                    preco_total=preco_total,
+                    comentario=fake.catch_phrase(),
+                    qtd_hospedes=qtd_hospedes,
+                )
+                r.save()
 
 
 create_db(50)
